84baa78657f10e0720adf918eada7e3d3429a2ff
student <student@itpark>
2022-05-29 19:06:08 +0300

    created server

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,23 @@
 package org.example;
 
-import com.github.javafaker.Faker;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+
+        int portNumber = 9999;
+
+        try (ServerSocket serverSocket = new ServerSocket(portNumber))
+        {
+            while (true){
+                final Socket socket = serverSocket.accept();
+                System.out.println("accepted");
+            }
+
+        } catch (IOException e) {
+        };
+
     }
 }
