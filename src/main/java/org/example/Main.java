fdabe3e15acd49ffdd31142e6920f5b234f42082
student <student@itpark>
2022-06-05 16:19:44 +0300

    Настройки параметров сервера

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
     public static void main(String[] args) throws DeadLineExceedException {
         final Server server = new Server();
+        server.setPort(7878);
         server.start();
     }
 }
